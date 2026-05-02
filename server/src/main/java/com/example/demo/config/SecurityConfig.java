48eb87bb8f04783995a491b8844d74f0ce935c1a
wing853 <qwqhsk@naver.com>
2026-05-02 19:23:21 +0900

    fix:SecurityConfig

--- server/src/main/java/com/example/demo/config/SecurityConfig.java
+++ server/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/users/signup", "/users/login").permitAll()
+                        .requestMatchers("/", "/users/signup", "/users/login","/recycle/analyze").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
